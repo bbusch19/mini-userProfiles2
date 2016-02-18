7ab0415e0319c3ffb48cde0cdb45cfe9cd535e82
Breiden Busch <breiden.busch@gmail.com>
2016-02-18 11:51:51 -0700

    Finished Project Day 3

--- controller.js
+++ controller.js
@@ -3,8 +3,8 @@ var app = angular.module('userProfiles');
 app.controller('MainController', function($scope, mainService, $http) {
 
   $scope.getUsers = function() {
-    mainService.getUsers().then(function(response) {
-      $scope.users = response.data.data;
+    mainService.getUsers().then(function(dataFromService) {
+      $scope.users = dataFromService;
     });
   }
   $scope.getUsers();
